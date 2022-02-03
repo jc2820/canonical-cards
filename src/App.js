bff42d58ef62d1f6601bba54330d8228aee9a663
Jamie Carter <jc2820@hotmail.com>
2022-02-03 11:19:50 +0000

    set up app.js to fetch api data, handle errors and map data array

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import './App.scss';
+import React from "react";
+import "./App.scss";
 
 function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+  const [error, setError] = React.useState(null);
+  const [isLoaded, setIsLoaded] = React.useState(false);
+  const [posts, setPosts] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch(
+      "https://people.canonical.com/~anthonydillon/wp-json/wp/v2/posts.json"
+    )
+      .then((res) => res.json())
+      .then(
+        (data) => {
+          setIsLoaded(true);
+          setPosts(data);
+          console.log(data)
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div className="App">
+        {posts.map((post) => (
+          <li key={post.id}></li>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
