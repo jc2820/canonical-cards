16162636757b00c04c5fc092369ee5b358fb2e49
Jamie Carter <jc2820@hotmail.com>
2022-02-08 17:26:37 +0000

    add new blog header and footer styles to fix alignment and borders

--- src/BlogPost/BlogPost.js
+++ src/BlogPost/BlogPost.js
@@ -5,19 +5,13 @@ import isoToStringDate from "../utils/dateUtils";
 function BlogPost({ post }) {
   return (
     <div className="blog-p-card--highlighted col-4">
-      <header className="p-card__header">
+      <header className="blog-p-card__header">
         <h4 className="p-muted-heading u-no-margin--bottom">
           Cloud and Server
         </h4>
       </header>
-      {/* <hr className="is-muted" /> */}
-
       <div className="p-card__content">
-        <img
-          className="blog-p-card__image"
-          alt=""
-          src={post.featured_media}
-        />
+        <img className="blog-p-card__image" alt="" src={post.featured_media} />
         <h3 className="p-heading--4">
           <a href={post.link}>{post.title.rendered}</a>
         </h3>
@@ -30,9 +24,8 @@ function BlogPost({ post }) {
             on {isoToStringDate(post.date)}
           </em>
         </p>
-        {/* <hr className="is-muted u-sv1" /> */}
       </div>
-      <footer className="p-card__footer">
+      <footer className="blog-p-card__footer">
         <p className="">Article</p>
       </footer>
     </div>
